8f5c7335b0a292f3f3a2bae12a982dd36bebbcb6
{Hoàng <{nguyenhoanganhvn90@gmail.com}>
2022-05-05 23:08:25 +0700

    removed dots link img

--- src/components/Home/config.js
+++ src/components/Home/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = window.location.origin + './img';
+export const baseUrl = window.location.origin + '/img';
 // 'img' is the images folder in 'public' (project_name/public/img/)

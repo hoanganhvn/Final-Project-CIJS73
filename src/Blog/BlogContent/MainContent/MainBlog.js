025322f9c8c4e0275adba1c44f205aadacd83021
{Hoàng <{nguyenhoanganhvn90@gmail.com}>
2022-05-07 09:17:53 +0700

    fixed link blog

--- src/Blog/BlogContent/MainContent/MainBlog.js
+++ src/Blog/BlogContent/MainContent/MainBlog.js
@@ -16,7 +16,7 @@ const MainBlog = () => {
                             </div>
                         </div>
                         <div className="col-md-8 col-xs-12 col-sm-12">
-                            <Link to="/tin-tuc/article1" className="margin-bottom" style={{ fontSize: '17px', fontWeight: 400 }}>7 loại giò không thể thiếu trong ngày Tết cổ truyền Việt Nam</Link>
+                            <Link to="/tin-tuc/gio" className="margin-bottom" style={{ fontSize: '17px', fontWeight: 400 }}>7 loại giò không thể thiếu trong ngày Tết cổ truyền Việt Nam</Link>
                             <div className="margin-bottom" style={{ fontSize: '14px', color: '#999' }}>Fresh Organic Food / 30.01.2022</div>
                             <p className="margin-bottom">Giò là món ăn truyền thống của người Việt Nam bởi nó vừa dân giã, vừa ngon miệng vừa sang trọng khi tiếp khách. Việt...</p>
                         </div>
@@ -28,7 +28,7 @@ const MainBlog = () => {
                             </div>
                         </div>
                         <div className="col-md-8 col-xs-12 col-sm-12">
-                            <Link to="/tin-tuc/article2" className="margin-bottom" style={{ fontSize: '17px', fontWeight: 400 }}>Nước uống ngon từ rau má, đơn giản và bổ dưỡng</Link>
+                            <Link to="/tin-tuc/rau-ma" className="margin-bottom" style={{ fontSize: '17px', fontWeight: 400 }}>Nước uống ngon từ rau má, đơn giản và bổ dưỡng</Link>
                             <div className="margin-bottom" style={{ fontSize: '14px', color: '#999' }}>Fresh Organic Food / 30.01.2022</div>
                             <p className="margin-bottom">Mùa hè nóng bức thế này chúng ta nên làm một cốc nước ép rau má cùng chút đá mát lạnh...</p>
                         </div>
@@ -40,7 +40,7 @@ const MainBlog = () => {
                             </div>
                         </div>
                         <div className="col-md-8 col-xs-12 col-sm-12">
-                            <Link to="/tin-tuc/article3" className="margin-bottom" style={{ fontSize: '17px', fontWeight: 400 }}>Công thức món tráng miệng giải ngấy dịp Tết</Link>
+                            <Link to="/tin-tuc/trang-mieng" className="margin-bottom" style={{ fontSize: '17px', fontWeight: 400 }}>Công thức món tráng miệng giải ngấy dịp Tết</Link>
                             <div className="margin-bottom" style={{ fontSize: '14px', color: '#999' }}>Fresh Organic Food / 30.01.2022</div>
                             <p className="margin-bottom">Sau một bữa ăn đầy ắp thịt cá, còn gì tuyệt vời hơn việc được thưởng thức những món tráng miệng ngon ngọt, mát lạnh đúng không?...</p>
                         </div>
@@ -52,7 +52,7 @@ const MainBlog = () => {
                             </div>
                         </div>
                         <div className="col-md-8 col-xs-12 col-sm-12">
-                            <Link to="/tin-tuc/article4" className="margin-bottom" style={{ fontSize: '17px', fontWeight: 400 }}>Làm báng ngọt kiểu Pháp từ nguyên liệu đơn giản</Link>
+                            <Link to="/tin-tuc/banh-ngot" className="margin-bottom" style={{ fontSize: '17px', fontWeight: 400 }}>Làm báng ngọt kiểu Pháp từ nguyên liệu đơn giản</Link>
                             <div className="margin-bottom" style={{ fontSize: '14px', color: '#999' }}>Fresh Organic Food / 30.01.2022</div>
                             <p className="margin-bottom">Nếu bạn đã thưởng thức bánh ngọt Pháp trước đây, rất có thể bạn đã thưởng thức crème pâtissière, hay còn gọi là kem bánh ngọt. Một khi bạn biết làm thế nào để làm cho một loại kem bánh ngọt cổ điển....</p>
                         </div>
--- src/Blog/BlogContent/Sidebar/NewsLatest/NewsLatest.js
+++ src/Blog/BlogContent/Sidebar/NewsLatest/NewsLatest.js
@@ -10,11 +10,11 @@ const NewsLatest = (props) => {
             </div>
             <div className="sidebar-article">
                 <div className="sidebar-image">
-                    <Link to="/blog/article1"><img src="https://raw.githubusercontent.com/hoanganhvn/Final-Project/master/Trang_Image/sap-mung-1-thanh-hoa-hong-mach-cach-lam-gio-ngu-sac-hinh-hoa-dep-lung-linh-de-thap-huong-6b4-5372426.jpg" alt="" /></Link>
+                    <Link to="/tin-tuc/gio"><img src="https://raw.githubusercontent.com/hoanganhvn/Final-Project/master/Trang_Image/sap-mung-1-thanh-hoa-hong-mach-cach-lam-gio-ngu-sac-hinh-hoa-dep-lung-linh-de-thap-huong-6b4-5372426.jpg" alt="" /></Link>
                 </div>
                 <div className="sidebar-post" style={{ width: '70%' }}>
                     <h3>
-                    <Link to="/blog/article1">
+                    <Link to="/tin-tuc/gio">
                     7 loại giò không thể thiếu trong ngày Tết cổ truyền Việt Nam
                     </Link>
                     </h3>
@@ -23,12 +23,12 @@ const NewsLatest = (props) => {
             </div>
             <div className="sidebar-article">
                 <div className="sidebar-image" style={{ width: '30%' }}>
-                <Link to="/blog/article2">
+                <Link to="/tin-tuc/rau-ma">
                     <img src="https://raw.githubusercontent.com/hoanganhvn/Final-Project/master/Trang_Image/blog07_large.jpg" alt="" /></Link>
                 </div>
                 <div className="sidebar-post" style={{ width: '70%' }}>
                     <h3>
-                    <Link to="/blog/article2">
+                    <Link to="/tin-tuc/rau-ma">
                     Nước uống ngon từ rau má, đơn giản và bổ dưỡng</Link>
                     </h3>
                     <span style={{ fontSize: '13px' }}>Fresh Organic Food 30.01.2022</span>
@@ -36,12 +36,12 @@ const NewsLatest = (props) => {
             </div>
             <div className="sidebar-article">
                 <div className="sidebar-image" style={{ width: '30%' }}>
-                <Link to="/blog/article3">
+                <Link to="/tin-tuc/trang-mieng">
                     <img src="https://raw.githubusercontent.com/hoanganhvn/Final-Project/master/Trang_Image/blog01_large.jpg" alt="" /></Link>
                 </div>
                 <div className="sidebar-post" style={{ width: '70%' }}>
                     <h3>
-                    <Link to="/blog/article3">
+                    <Link to="/tin-tuc/trang-mieng">
                     Công thức món tráng miệng giải ngấy dịp Tết</Link>
                     </h3>
                     <span style={{ fontSize: '13px' }}>Fresh Organic Food 30.01.2022</span>
@@ -49,12 +49,12 @@ const NewsLatest = (props) => {
             </div>
             <div className="sidebar-article">
                 <div className="sidebar-image" style={{ width: '30%' }}>
-                <Link to="/blog/article4">
+                <Link to="/tin-tuc/banh-ngot">
                     <img src="https://raw.githubusercontent.com/hoanganhvn/Final-Project/master/Trang_Image/blog02_large.jpg" alt="" /></Link>
                 </div>
                 <div className="sidebar-post" style={{ width: '70%' }}>
                     <h3>
-                    <Link to="/blog/article4">
+                    <Link to="/tin-tuc/banh-ngot">
                     Làm báng ngọt kiểu Pháp từ nguyên liệu đơn giản</Link>
                     </h3>
                     <span style={{ fontSize: '13px' }}>Fresh Organic Food 30.01.2022</span>
